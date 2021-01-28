6366d7c5d651e3359e92e993d9d793a2c1eb3e73
Pulkit Chaudhary <pulkithb@gmail.com>
2021-01-28 20:58:14 +0530

    Update_1 adding files

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const db = knex({
+	client: 'pg',
+	connection:{
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'test',
+		database: 'postgres'
+	}
+}); 
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+
+
+/*app.get('/',(req,res)=>{
+    res.send(database.users)
+})*/
+app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)})
+
+app.post('/register',(req,res) => {register.handleRegister(req,res,db,bcrypt)})
+
+app.get('/profile/:id',(req,res) => {profile.handleProfile(req,res,db)})
+
+app.put('/image',(req,res)=>{image.handleImage(req,res,db)});
+
+app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res )});
+
+
+
+// Load hash from your password DB.
+/**/
+
+app.listen(3001,()=>{
+    console.log('app is running  on port 3001')
+})
+
